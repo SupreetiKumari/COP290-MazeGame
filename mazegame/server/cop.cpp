0fe89cf9a7b14fe2022828d43f5b9df36e7daf66
Supreeti Kumari <78208708+SupreetiKumari@users.noreply.github.com>
2022-05-03 20:28:26 +0530

    Update cop.cpp

--- mazegame/server/cop.cpp
+++ mazegame/server/cop.cpp
@@ -86,3 +86,64 @@ SDL_Texture* gTexture = NULL;
 LTexture gDotTexture;
 LTexture gDot2Texture;
 LTexture instruction1;
+
+void Dot::handleEvent( SDL_Event& e )
+{
+    //If a key was pressed
+    if( e.type == SDL_KEYDOWN && e.key.repeat == 0 )
+    {
+        //Adjust the velocity
+        switch( e.key.keysym.sym )
+        {
+            case SDLK_UP: mVelY -= DOT_VEL; break;
+            case SDLK_DOWN: mVelY += DOT_VEL; break;
+            case SDLK_LEFT: mVelX -= DOT_VEL; break;
+            case SDLK_RIGHT: mVelX += DOT_VEL; break;
+        }
+    }
+    //If a key was released
+    else if( e.type == SDL_KEYUP && e.key.repeat == 0 )
+    {
+        //Adjust the velocity
+        switch( e.key.keysym.sym )
+        {
+            case SDLK_UP: mVelY += DOT_VEL; break;
+            case SDLK_DOWN: mVelY -= DOT_VEL; break;
+            case SDLK_LEFT: mVelX += DOT_VEL; break;
+            case SDLK_RIGHT: mVelX -= DOT_VEL; break;
+        }
+    }
+}
+
+
+void Dot::move1()
+{
+    // cout<<cpoint<<"\n";
+     //Move the dot left or right
+     mPosX += mVelX;mPosY += mVelY;
+     PlayerCollider.x = mPosX;
+     PlayerCollider.y = mPosY;
+     //If the dot went too far to the left or right
+     if( ( mPosX < 0 ) || ( mPosX + DOT_WIDTH > SCREEN_WIDTH ) )
+     {
+         //Move back
+         mPosX -= mVelX; PlayerCollider.x = mPosX;
+     }
+//     else if(checkCollision( PlayerCollider, walls )){
+//         mPosX -= mVelX; PlayerCollider.x = mPosX;
+//     }
+     //Move the dot up or down
+     
+     //If the dot went too far up or down
+     if( ( mPosY < 0 ) || ( mPosY + DOT_HEIGHT > SCREEN_HEIGHT ) )
+     {
+         //Move back
+         mPosY -= mVelY;
+         PlayerCollider.y = mPosY;
+        
+     }
+//     else if(checkCollision( PlayerCollider, walls )){
+//         mPosY -= mVelY;
+//         PlayerCollider.y = mPosY;
+//     }
+}
