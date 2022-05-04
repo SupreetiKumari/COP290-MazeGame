ab0fd937c41d789a728c21b47ed838e0815ec051
Supreeti Kumari <78208708+SupreetiKumari@users.noreply.github.com>
2022-05-04 19:11:18 +0530

    Update sdl_helper.cpp

--- mazegame/server/sdl_helper.cpp
+++ mazegame/server/sdl_helper.cpp
@@ -267,21 +267,21 @@ void LTexture::setAlpha( Uint8 alpha )
 }
 
 
-// void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
-// {
-//     //Set rendering space and render to screen
-//     SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
+{
+    //Set rendering space and render to screen
+    SDL_Rect renderQuad = { x, y, mWidth, mHeight };
 
-//     //Set clip rendering dimensions
-//     if( clip != NULL )
-//     {
-//         renderQuad.w = clip->w;
-//         renderQuad.h = clip->h;
-//     }
+    //Set clip rendering dimensions
+    if( clip != NULL )
+    {
+        renderQuad.w = clip->w;
+        renderQuad.h = clip->h;
+    }
 
-//     //Render to screen
-//     SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip );
-// }
+    //Render to screen
+    SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip );
+}
 
 int LTexture::getWidth()
 {
