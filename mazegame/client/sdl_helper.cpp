e7eebdbd39d72a579775d248b4b2db69cd58fd48
Arushi29goyal <77873184+Arushi29goyal@users.noreply.github.com>
2022-05-05 21:15:23 +0530

    Update sdl_helper.cpp

--- mazegame/client/sdl_helper.cpp
+++ mazegame/client/sdl_helper.cpp
@@ -146,3 +146,60 @@ class Dot
         //The velocity of the dot
         int mVelX, mVelY;
 };
+
+LTexture::LTexture()
+{
+    //Initialize
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
+}
+
+
+LTexture::~LTexture()
+{
+    //Deallocate
+    free();
+}
+
+SDL_Renderer* gRenderer = NULL;
+bool LTexture::loadFromFile( std::string path )
+{
+//Get rid of preexisting texture
+free();
+
+//The final texture
+SDL_Texture* newTexture = NULL;
+
+//Load image at specified path
+SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+if( loadedSurface == NULL )
+{
+    printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+}
+else
+{
+    //Color key image
+    SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+
+    //Create texture from surface pixels
+    newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+    if( newTexture == NULL )
+    {
+        printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+    }
+    else
+    {
+        //Get image dimensions
+        mWidth = loadedSurface->w;
+        mHeight = loadedSurface->h;
+    }
+
+    //Get rid of old loaded surface
+    SDL_FreeSurface( loadedSurface );
+}
+
+//Return success
+mTexture = newTexture;
+return mTexture != NULL;
+}
