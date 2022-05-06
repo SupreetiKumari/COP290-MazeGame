3c86f49586c07c4c39bec1b08f9317aa48aa16f9
Arushi29goyal <77873184+Arushi29goyal@users.noreply.github.com>
2022-05-06 11:50:51 +0530

    Update cop.cpp

--- mazegame/client/cop.cpp
+++ mazegame/client/cop.cpp
@@ -614,3 +614,19 @@ bool loadMedia()
     return success;
 }
 
+	void close()
+{
+	//Free loaded images
+	gDotTexture.free();
+
+	//Destroy window	
+	SDL_DestroyRenderer( gRenderer );
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
+
